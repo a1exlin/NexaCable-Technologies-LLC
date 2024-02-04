4057e388c971e3bc6cefb252c9b894d9ff85c9a6
a1exlin <104739337+a1exlin@users.noreply.github.com>
2024-02-04 18:33:46 -0500

    added axios and added https for backend server

--- my-app/src/components/contact.js
+++ my-app/src/components/contact.js
@@ -16,7 +16,7 @@ function Contact() {
     const handleSubmit = (e) => {
         e.preventDefault();
     
-        axios.post('http://nexacable.net/api/contactForm.php', { firstname, email, number, lastname, message })
+        axios.post('https://nexacable.net/api/contactForm.php', { firstname, email, number, lastname, message })
             .then(response => {
                 console.log('Inquiry sent successfully!', response.data);
             })
@@ -36,10 +36,10 @@ function Contact() {
                     </div>
                     <p className="contact-subheader"> Use the form below to submit requests and/or questions. We will get back to you as soon as possible. </p>
                     <div className='contact1'>
-                        <input placeholder='First Name' className='firstname'type='firstname' onChange={(e) => setFirstname(e.target.value)}></input>
+                        <input placeholder='First Name' className='firstname-input'type='firstname' onChange={(e) => setFirstname(e.target.value)}></input>
                     </div>
                     <div className='contact2'>
-                        <input placeholder='Last Name' className='lastname' type='lastname'autoComplete="off"onChange={(e) => setLastname(e.target.value)}></input>
+                        <input placeholder='Last Name' className='lastname-input' type='lastname'autoComplete="off"onChange={(e) => setLastname(e.target.value)}></input>
                     </div>
                     <div className='contact3'>
                         <input placeholder='Email' className='email-input'type='email'autoComplete="off" onChange={(e) => setEmail(e.target.value)}></input>
