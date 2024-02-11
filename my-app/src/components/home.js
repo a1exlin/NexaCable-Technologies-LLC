d5c20cd35a12e19919f3fe973054f9883afb1eb9
Alexander Phillipson <127406648+AJPNetworks@users.noreply.github.com>
2024-02-11 00:46:49 -0500

    im tired lol

--- my-app/src/components/home.js
+++ my-app/src/components/home.js
@@ -16,15 +16,15 @@ function home() {
             <div className="home-img-wrapper">
                 <img src={logo} alt-="home-image" className='home-img'/>
             </div>
-
+            
             <div className="home">
                 <h1 className="header1">NexaCable</h1>
                 <h1 className="header2">Technologies</h1>
-                <p1 className="provide-txt">We provide commercial network and security systems installation </p1>
-                <p className="provide-txt2">for large and small businesses or new residential constructions </p>
+                <p className="provide-txt">We provide commercial network and security systems installation</p>
+                <p className="provide-txt2">for large and small businesses or new residential constructions</p>
             </div>
             
-            <button className="quote"> REQUEST A QUOTE <GoArrowRight className="arrow" /></button>
+            <button className="quote">REQUEST A QUOTE<GoArrowRight className="arrow" /></button>
         </div>
     )
 }
